48925de1efc2222f02ba02ed8a8797fca0177a48
Michael Adam <madamt0001@gmail.com>
2017-04-20 12:59:09 -0800

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,7 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/custom.css') }}">
     </head>
     <body>
         <div class="container-fluid">
