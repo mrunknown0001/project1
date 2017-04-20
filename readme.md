4a6ebed34882c3b4af2fdc07b68ecedd9ce001b6
Michael Adam <madamt0001@gmail.com>
2017-04-20 12:49:32 -0800

    Created Login form

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-## Project OOC/PGI using Laravel 5.4
+## Project1 OOC/PGI using Laravel 5.4
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>OOC/PGI</title>
+        <title>Login - Project1</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -13,6 +13,30 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        
+        <div class="container-fluid">
+            <hr>
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <strong>Login to Project1</strong>
+                        </div>
+                        <div class="panel-body">
+                            <form action="">
+                                <div class="form-group">
+                                    <input type="text" name="username" id="username" placeholder="Username" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" id="password" placeholder="Enter Password" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <button class="btn btn-primary">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
